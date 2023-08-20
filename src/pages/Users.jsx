5c6b94151111c2d82e5f9e0a5da16424a8544464
saifulaija <saifulaija2023@gmail.com>
2023-08-20 21:42:58 +0600

    some style added

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -8,7 +8,7 @@ import Loader from "../components/Loader";
 import EmptyState from "./EmptyState";
 
 const Users = () => {
-      const navigation=useNavigation()
+  const navigation = useNavigation();
   const dispatch = useDispatch();
   const users = useSelector((state) => state.users.users);
   useEffect(() => {
@@ -36,32 +36,44 @@ const Users = () => {
       });
   };
   if (navigation.state === "loading") {
-      return <Loader></Loader>;
-    }
+    return <Loader></Loader>;
+  }
 
   return (
-   <div className="container mx-auto my-20">
-      {users.length > 0? (<> <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3  gap-4 p-4">
-      {users.map((user) => (
-        <div
-          key={user.id}
-          className="bg-white p-4 rounded-lg border border-gray-300"
-        >
-          <h2 className="text-lg font-semibold">{user.name}</h2>
-          <p className="text-gray-600 mb-2">Email: {user.email}</p>
-          <p className="text-gray-600 mb-2">Age: {user.age}</p>
-          <div className="flex justify-end">
-            <button onClick={() => handleDelete(user.id)}>
-              <RiDeleteBin5Line className="text-red-600 cursor-pointer mr-2" />
-            </button>
-            <Link to={`/update/${user.id}`}>
-              <RiPencilLine className="text-blue-600 cursor-pointer" />
-            </Link>
-          </div>
+    <div className="container mx-auto my-20">
+      {users.length > 0 ? (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3  gap-6">
+          {users.map((user) => (
+            <div
+              key={user.id}
+              className="bg-white p-4 rounded-lg shadow-md border border-gray-300"
+            >
+              <h2 className="text-lg font-semibold mb-2">{user.name}</h2>
+              <p className="text-gray-600 mb-2">Email: {user.email}</p>
+              <p className="text-gray-600 mb-2">Age: {user.age}</p>
+              <div className="flex justify-end mt-4">
+                <button
+                  onClick={() => handleDelete(user.id)}
+                  className="flex items-center px-3 py-1 bg-red-500 text-white rounded-md hover:bg-red-600 focus:outline-none focus:ring focus:ring-red-300"
+                >
+                  <RiDeleteBin5Line className="mr-1" />
+                  Delete
+                </button>
+                <Link
+                  to={`/update/${user.id}`}
+                  className="flex items-center px-3 py-1 ml-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-300"
+                >
+                  <RiPencilLine className="mr-1" />
+                  Edit
+                </Link>
+              </div>
+            </div>
+          ))}
         </div>
-      ))}
-    </div></>):<EmptyState></EmptyState>}
-   </div>
+      ) : (
+        <EmptyState />
+      )}
+    </div>
   );
 };
 
